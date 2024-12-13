fa74ce70878050d75a222e153fe5bb64516ab386
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-12-13 11:14:39 +0100

    done

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 app.use("/api/users", usersRouter);
 
+app.use("/avatars", express.static("public/avatars"));
+
 app.use((req, res) => {
 	res.status(404).json({ message: "Not found" });
 });
